a435017de60a49acead7b88fe3668b6a1feb16ee
SamuelMorries2002 <107941025+SamuelMorries2002@users.noreply.github.com>
2023-11-07 23:18:55 -0500

    Update traffic_generator.py

--- traffic_generator.py
+++ traffic_generator.py
@@ -6,23 +6,23 @@ def run_traffic():
 
 # ICMP for connectivity testing
  ping = IP(dst=target_ip)/ICMP()  
- sendp(ping,count=50)
+ sendp(ping,count=50,iface="Wi-Fi")
 
 #ICMP Destination Unreachable
  unreach = IP(dst=target_ip, proto=1)/ICMP(type=3, code=3) 
- sendp(unreach, count=25)
+ sendp(unreach, count=25,iface="Wi-Fi")
 
 # IGMP for multicast testing
  query = IP(dst=target_ip, proto=2)/IGMP(type=0x11)
- sendp(query, count=10)
+ sendp(query, count=10,iface="Wi-Fi")
 
 # IP testing
  frag = IP(dst=target_ip, frag=1, proto=1)/ICMP()/("X"*1400)
- sendp(frag, count=50) 
+ sendp(frag, count=50,iface="Wi-Fi") 
 
  proto = IP(dst=target_ip, proto=255)/UDP()
- sendp(proto, count=10)
+ sendp(proto, count=10,iface="Wi-Fi")
 
 # ARP testing
  arp = ARP(pdst=target_ip, opcode=1) 
- sendp(arp, count=30)
+ sendp(arp, count=30,iface="Wi-Fi")
